5211f4dc687a3c26310518ff9e1d83987127c504
William_A <56189341+Will989151@users.noreply.github.com>
2019-11-02 18:12:10 -0600

    added javascript code for key input, guesses, etc. Game still not functional :(

---
+++ assets/javascript/game.js
@@ -0,0 +1,47 @@
+var wins = 0;
+var losses = 0;
+var guesses = 10;
+var guessedArray = [];
+var randomLetter;
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+randomLetter = letters[Math.floor(Math.random() *letters.length)];
+console.log(randomLetter);
+
+function letterSelector() {
+    randomLetter = letters[Math.floor(Math.random() *letters.length )];
+    console.log(randomLetter);
+}
+
+document.onkeyup = function(event) {
+    var playersChoice = event.key;
+
+    if (playersChoice === randomLetter) {
+        wins++;
+        guesses = 10;
+        guessedArray = [];
+    }
+
+    letterSelector();
+    if  (playersChoice !== randomLetter) {
+        guesses--;
+    }
+
+    if (guesses === 0) {
+        losses++;
+        guessedArray = [];
+        guesses = 10;
+    }
+
+    if (guessedArray.indexOf(guesses) >= 0) {
+
+    }  else {
+        guessedArray.push(guesses);
+        
+        document.getElementById("Guesses Left").innerHTML = guesses;
+        document.getElementById("Wins").innerHTML = wins;
+        document.getElementById("Losses").innerHTML = losses;
+        document.getElementById("Your Guesses So Far").innerHTML = guessedArray;
+    }
+
+}
